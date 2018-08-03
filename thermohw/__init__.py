7bb5ae6e5d4873089df39d28dca6894ce05253e3
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-08-03 12:23:33 -0400

    Export appropriate classes from the package __init__.py

---
+++ thermohw/__init__.py
@@ -0,0 +1,4 @@
+from .convert_thermo_hw import (assignment_nb_exp, solution_nb_exp, assignment_pdf_exp,
+                                solution_pdf_exp)
+from .extract_attachments import ExtractAttachmentsPreprocessor
+from ._version import __version__
