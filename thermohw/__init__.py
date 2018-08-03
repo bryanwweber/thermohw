3a59e6be8a6d39d0cd9b5b1eefab29275a108715
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-08-03 12:30:06 -0400

    Add docstring to __init__.py

--- thermohw/__init__.py
+++ thermohw/__init__.py
@@ -1,4 +1,6 @@
-from .convert_thermo_hw import (assignment_nb_exp, solution_nb_exp, assignment_pdf_exp,
-                                solution_pdf_exp)
-from .extract_attachments import ExtractAttachmentsPreprocessor
-from ._version import __version__
+"""Convert thermo homework assignments."""
+
+from .convert_thermo_hw import (assignment_nb_exp, solution_nb_exp,  # noqa: F401
+                                assignment_pdf_exp, solution_pdf_exp)  # noqa: F401
+from .extract_attachments import ExtractAttachmentsPreprocessor  # noqa: F401
+from ._version import __version__  # noqa: F401
