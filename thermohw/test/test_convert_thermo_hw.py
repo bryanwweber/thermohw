03e61ecb6130b59601c4e967ce53b3784324de2f
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-09-03 13:50:00 -0400

    Fix path to test file in test_convert_thermo_hw

--- thermohw/test/test_convert_thermo_hw.py
+++ thermohw/test/test_convert_thermo_hw.py
@@ -1,16 +1,14 @@
 """Test the convert_thermo_hw module."""
 import os
+import pkg_resources
 
 from ..convert_thermo_hw import solution_pdf_exp
-
-here = os.path.dirname(__file__)
 
 
 def test_convert_pathological_image_name():
     """Test that converting a notebook with a pathological image name works."""
+    filename = os.path.join('test-pathological-image-name.ipynb')
+    filename = pkg_resources.resource_filename(__name__, filename)
     res = {'unique_key': 'test-pathological-image-name', 'by_hand': False}
-    solution_pdf, _ = solution_pdf_exp.from_filename(
-        os.path.join(here, 'test-pathological-image-name.ipynb'),
-        res,
-    )
+    solution_pdf, _ = solution_pdf_exp.from_filename(filename, res)
     assert len(solution_pdf) > 0
