4f761b5628764be2b6b5a0b66e190e3af7ddfea6
Bryan W. Weber <bryan.w.weber@gmail.com>
2020-09-03 13:47:57 -0400

    :gem: Release 0.5.1

--- thermohw/test/test_convert_thermo_hw.py
+++ thermohw/test/test_convert_thermo_hw.py
@@ -2,13 +2,17 @@
 import os
 import pkg_resources
 
-from ..convert_thermo_hw import solution_pdf_exp
+from ..convert_thermo_hw import pdf_exp
 
 
-def test_convert_pathological_image_name():
+def test_convert_pathological_image_name() -> None:
     """Test that converting a notebook with a pathological image name works."""
-    filename = os.path.join('test-pathological-image-name.ipynb')
+    filename = os.path.join("test-pathological-image-name.ipynb")
     filename = pkg_resources.resource_filename(__name__, filename)
-    res = {'unique_key': 'test-pathological-image-name', 'by_hand': False}
-    solution_pdf, _ = solution_pdf_exp.from_filename(filename, res)
+    res = {
+        "unique_key": "test-pathological-image-name",
+        "by_hand": False,
+        "remove_solution": False,
+    }
+    solution_pdf, _ = pdf_exp.from_filename(filename, res)
     assert len(solution_pdf) > 0
--- thermohw/test/test_extract_attachments.py
+++ thermohw/test/test_extract_attachments.py
@@ -1,5 +1,5 @@
 """Test the extract_attachments preprocessor."""
-from nbformat.v4 import new_markdown_cell  # type: ignore
+from nbformat.v4 import new_markdown_cell
 from binascii import a2b_base64
 
 from ..extract_attachments import ExtractAttachmentsPreprocessor
@@ -8,16 +8,18 @@ from ..extract_attachments import ExtractAttachmentsPreprocessor
 preproc = ExtractAttachmentsPreprocessor()
 
 
-def test_pathological_image_name():
+def test_pathological_image_name() -> None:
     """Test that image name with escaped characters is converted properly."""
-    fname = "%28%29%40%23%24%25%5E%26%2A%21%3C%3E%3F%2C%7B%7D%5B%5D%5C%7C%7E%60%2B%3D%20"
-    repl = '-'*(len(fname)//3)
-    data = ("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB"
-            "0C8AAAAASUVORK5CYII=")
-    in_cell = new_markdown_cell(
-        source=(f"![{fname}.png](attachment:{fname}.png)")
+    fname = (
+        "%28%29%40%23%24%25%5E%26%2A%21%3C%3E%3F%2C%7B%7D%5B%5D%5C%7C%7E%60%2B%3D%20"
     )
-    in_cell['attachments'] = {f"{fname}.png": {"image/png": data}}
-    cell, resources = preproc.preprocess_cell(in_cell, {'outputs': {}}, 0)
+    repl = "-" * (len(fname) // 3)
+    data = (
+        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB"
+        "0C8AAAAASUVORK5CYII="
+    )
+    in_cell = new_markdown_cell(source=(f"![{fname}.png](attachment:{fname}.png)"))
+    in_cell["attachments"] = {f"{fname}.png": {"image/png": data}}
+    cell, resources = preproc.preprocess_cell(in_cell, {"outputs": {}}, 0)
     assert cell.source == f"![{fname}.png](_0_{repl}.png)"
-    assert resources['outputs'][f'_0_{repl}.png'] == a2b_base64(data)
+    assert resources["outputs"][f"_0_{repl}.png"] == a2b_base64(data)
